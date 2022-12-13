9a2c218d1034c96e6be15d2adfdb86f13893b49e
dpwns1234 <dpwns1234@naver.com>
2022-12-13 17:20:17 +0900

    feat: 전체적인 흐름에 따라 주석으로 로직 완성

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,20 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == LOTTO_COUNT)
     }
 
     // TODO: 추가 기능 구현
+    fun makeLotto(): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(RANDOM_START_INCLUSIVE, RANDOM_END_INCLUSIVE, LOTTO_COUNT)
+    }
+
+    companion object {
+        const val RANDOM_START_INCLUSIVE = 1
+        const val RANDOM_END_INCLUSIVE = 45
+        const val LOTTO_COUNT = 6
+    }
 }
---
+++ src/main/kotlin/lotto/LottoController.kt
@@ -0,0 +1,36 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Console
+import lotto.model.LottoComparator
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class LottoController {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+
+    fun runGame() {
+        println("구입금액을 입력해 주세요.")
+        val input = Console.readLine()
+        // 입력값 체크
+
+        // 로또 생성 TODO (어디에 저장할 것인가?)
+        for(i in 0 until input/1000) {
+            Lotto().makeLotto()
+        }
+
+        println("당첨 번호를 입력해 주세요.")
+        val winningLotto = Console.readLine()
+        // 입력값 체크
+
+        // convert string to list
+
+        println("보너스 번호를 입력해 주세요.")
+        val bonusNumber = Console.readLine()
+        // 중복 체크 등 입력값 체크
+
+        // 당첨 결과 계산
+        LottoComparator()
+        println("당첨 통계")
+    }
+}
